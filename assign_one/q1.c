174061dd369e042ef4320d80b249d9ac200c43a3
Shreedutt Tiwari <77563992+Tshreedutt@users.noreply.github.com>
2021-08-24 20:11:43 +0530

    Delete assign_one directory

--- assign_one/q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a1,a2,a3;
-    scanf("%d%d%d",&a1,&a2,&a3);
-    if(a1>a2 && a1>a3) printf("\n%d",a1);
-    else if(a2>a3 && a2>a1) printf("\n%d",a2);
-         else printf("\n%d",a3);
-    return 0;
-}
